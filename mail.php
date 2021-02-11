4be9614d29eab8f6a4fa9ca06c9e624426e3ac20
Chekube <mdankaro@gmail.com>
2021-02-11 10:46:33 +0100

    bike_repairshop

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+
+
+
+$content="From: $name";
+$recipient = "dan@example.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("error");
+echo "Email sent:";
+?>
